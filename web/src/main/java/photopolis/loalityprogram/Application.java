6df2d454dbc58e7ee419959a9c30ecde70d6e2fe
Anatoliy <govier999@gmail.com>
2018-05-23 17:55:23 +0300

    config

---
+++ web/src/main/java/photopolis/loalityprogram/Application.java
@@ -0,0 +1,23 @@
+package photopolis.loalityprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+/**
+ * Created by Anatoliy on 23.05.2018.
+ */
+
+@SpringBootApplication(scanBasePackages  = "photopolis.loalityprogram")
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+}
