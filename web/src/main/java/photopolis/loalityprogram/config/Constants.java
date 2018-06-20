b4aa78952ebc4dd38b54d50b3eaef6d05ef986ca
Anatoliy <govier999@gmail.com>
2018-06-20 18:15:32 +0300

    Update Constants.java

--- web/src/main/java/photopolis/loalityprogram/config/Constants.java
+++ web/src/main/java/photopolis/loalityprogram/config/Constants.java
@@ -23,7 +23,7 @@ public class Constants {
     public static final String PASSWORD = "12345678";
 
     public static final String BOT_USERNAME="loality_program_bot";
-    public static final String BOT_TOKEN="611013171:AAGH5QWmxkaJi23AamxyWmlSNQSS1t2dVTg";
+    public static final String BOT_TOKEN="";//TODO ADD TOKEN KEY
     public static final String BOT_SENDMESSAGE_URL="https://api.telegram.org/bot611013171:AAGH5QWmxkaJi23AamxyWmlSNQSS1t2dVTg/sendMessage?";
     public static String BOT_CHAT_ID="324492499";
 
